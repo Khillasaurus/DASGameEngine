280f63df31f0b5358bc1e77b43b9a9a365fba4d8
Khillasaurus <D.A.Schenker@gmail.com>
2015-07-14 02:09:13 -0600

    2015.02.23 Going to start object hierarchy
    Added Object Hierarchy Planning notes, as going forward I wanted to have
    a precise design layout in mind.
    I also did some formatting and cleanup in Application.cpp in preparation
    for this.

--- root/Application/Main/Application.cpp
+++ root/Application/Main/Application.cpp
@@ -416,7 +416,7 @@ void Application::LoadModelAssetSpaceship()
 {
 	//Vertices
 
-	const unsigned int kVertexCount = 10;
+	const unsigned int kVertexCount = 23;
 	const unsigned int kPositionDimensions = 3;
 	const unsigned int kTextureDimensions = 2;
 	const unsigned int kColorDimensions = 4;
@@ -425,45 +425,137 @@ void Application::LoadModelAssetSpaceship()
 	GLfloat* pVertices = new GLfloat[kVertexCount * kDataBitsPerVertex]
 	{
 		/*
-											_														//complicated version for later
-		      .				      1			|							      .						|      .
-		     / \			     / \		|		Length:	10m			     /|\					|     / \
-		    / . \			    8 0 2		|	}	Width:	8m			    /_|_\					|   _/   \_
-		  /_     _\			  7_6 9 4_3		|		Height:	2.5m		  /__\|/__\					|  / _   _ \
-		     \./			     \5/		|							     \./					|  \/ \./ \/
+		OVERALL
+		//TODO: Draw the overall ship design using ascii art.
+
+		FLOOR
+						2		_
+		   /\		   /\		|		Length:	 8m
+		  /  \		  /  \		|	}	Width:	 4m
+		 /____\		0/____\1	|		Height:	-1m
+								-
+
+
+		MAIN LEVEL
+											_
+						6					|
+					  /   \					|
+					 /  5  \				|
+					/ 3   4 \				|		Length:	10m	
+				   / 1     2 \				|	}	Width:	8m	
+				 9/           \0			|		Height:	2.5m
+			    /   7       8   \			|						//Note: Vertices 7 and 8 are slightly elevated.
+			 3/______4_____5______\6		|
 											-
+
+
+		UPPER ACCENTS
+
+						x
+					  /   \
+					 /  x  \
+					/ x1 2x \
+				   / x 9 0 x \
+				 x/           \x
+			    / 7 x       x 8 \
+			 x/______x_____x______\x
+
 		*/
 
 		//Position																								Texture						Color
 		//x					y					z																							r		g		b		a
 
-		 0.0f,				 2.5f * kDCPerM,	 0.0f,															1.0f,	1.0f,				0.4f,	0.9f,	1.0f,	1.0f,	//0:	Cockpit
-		 0.0f,				 0.0f,				-5.0f * kDCPerM,												1.0f,	1.0f,				0.8f,	0.2f,	1.0f,	1.0f,	//1:	Bow (Nose)
-		 2.0f * kDCPerM,	 0.0f,				 0.0f,															1.0f,	1.0f,				0.4f,	1.0f,	0.55f,	1.0f,	//2:	Starboard (Right)
-		 4.0f * kDCPerM,	 0.0f,				 2.5f * kDCPerM,												1.0f,	1.0f,				0.9f,	0.9f,	0.1f,	1.0f,	//3:	Right Wing Tip
-		 2.0f * kDCPerM,	 0.0f,				 2.5f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.5f,	0.5f,	1.0f,	//4:
-		 0.0f,				 0.0f,				 5.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.35f,	0.1f,	1.0f,	//5:	Stern (Tail)
-		-2.0f * kDCPerM,	 0.0f,				 2.5f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.5f,	0.5f,	1.0f,	//6:
-		-4.0f * kDCPerM,	 0.0f,				 2.5f * kDCPerM,												1.0f,	1.0f,				0.9f,	0.9f,	0.1f,	1.0f,	//7:	Left Wing Tip
-		-2.0f * kDCPerM,	 0.0f,				 0.0f,															1.0f,	1.0f,				0.4f,	1.0f,	0.55f,	1.0f,	//8:	Port (Left)
-		 0.0f,				 0.0f,				 2.5f * kDCPerM,												1.0f,	1.0f,				0.4f,	0.35f,	0.45f,	1.0f	//9:	Amidships
+		//FLOOR
+		-2.0f * kDCPerM,	-1.0f * kDCPerM,	 4.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	0.0f,	1.0f,	//0
+		 2.0f * kDCPerM,	-1.0f * kDCPerM,	 4.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	0.0f,	1.0f,	//1
+		 0.0f,				-1.0f * kDCPerM,	-4.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	0.0f,	1.0f,	//2
+
+		//MAIN LEVEL
+		-4.0f * kDCPerM,	 0.0f,				 5.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//3
+		-1.0f * kDCPerM,	 0.0f,				 5.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//4
+		 1.0f * kDCPerM,	 0.0f,				 5.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//5
+		 4.0f * kDCPerM,	 0.0f,				 5.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//6
+		-1.5f * kDCPerM,	 0.5f * kDCPerM,	 3.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//7
+		 1.5f * kDCPerM,	 0.5f * kDCPerM,	 3.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//8
+		-2.0f * kDCPerM,	 0.0f,				 1.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//9
+		 2.0f * kDCPerM,	 0.0f,				 1.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//10
+		-0.8f * kDCPerM,	 0.0f,				 0.5f * kDCPerM,												1.0f,	1.0f,				0.0f,	0.0f,	1.0f,	1.0f,	//11
+		 0.8f * kDCPerM,	 0.0f,				 0.5f * kDCPerM,												1.0f,	1.0f,				0.0f,	0.0f,	1.0f,	1.0f,	//12
+		-1.0f * kDCPerM,	 0.0f,				-0.75f * kDCPerM,												1.0f,	1.0f,				0.0f,	0.0f,	1.0f,	1.0f,	//13
+		 1.0f * kDCPerM,	 0.0f,				-0.75f * kDCPerM,												1.0f,	1.0f,				0.0f,	0.0f,	1.0f,	1.0f,	//14
+		 0.0f,				 0.0f,				-2.0f * kDCPerM,												1.0f,	1.0f,				0.0f,	0.0f,	1.0f,	1.0f,	//15
+		 0.0f,				 0.0f,				-5.0f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	1.0f,	1.0f,	//16
+
+		//UPPER ACCENTS
+		-2.5f * kDCPerM,	 1.5f * kDCPerM,	 3.67f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	0.0f,	1.0f,	//17
+		 2.5f * kDCPerM,	 1.5f * kDCPerM,	 3.67f * kDCPerM,												1.0f,	1.0f,				1.0f,	0.0f,	0.0f,	1.0f,	//18
+		-0.5f * kDCPerM,	 1.0f * kDCPerM,	 0.0f,															1.0f,	1.0f,				0.0f,	1.0f,	1.0f,	1.0f,	//19
+		 0.5f * kDCPerM,	 1.0f * kDCPerM,	 0.0f,															1.0f,	1.0f,				0.0f,	1.0f,	1.0f,	1.0f,	//20
+		-0.5f * kDCPerM,	 1.0f * kDCPerM,	-0.67f * kDCPerM,												1.0f,	1.0f,				0.0f,	1.0f,	1.0f,	1.0f,	//21
+		 0.5f * kDCPerM,	 1.0f * kDCPerM,	-0.67f * kDCPerM,												1.0f,	1.0f,				0.0f,	1.0f,	1.0f,	1.0f	//22
 	};
 
 	//Elements
 
 	const unsigned int kElementsPerDrawType = 3;//Triangle
-	const unsigned int kDrawTypeElements = 7;
+	const unsigned int kDrawTypeElements = 42;
 	const unsigned int kElementCount = kElementsPerDrawType * kDrawTypeElements;
 
 	GLuint* pElements = new GLuint[kElementCount]
 	{
-		0, 2, 1,
-		0, 8, 1,
-		0, 9, 2,
-		0, 9, 8,
-		9, 3, 2,
-		9, 7, 8,
-		4, 6, 5
+		//FLOOR (Count: 1)
+		0, 1, 2,
+
+		//FLOOR - MAIN LEVEL (Count: 10)
+		0, 3, 4,
+		0, 3, 9,
+		0, 2, 9,
+		0, 4, 5,
+		1, 5, 0,
+		1, 5, 6,
+		1, 10, 6,
+		1, 10, 2,
+		2, 16, 9,
+		2, 16, 10,
+
+		//ROOF (Count: 31)
+		// Left Wing (Count: 4)
+		17, 3, 4,
+		17, 7, 4,
+		17, 7, 9,
+		17, 3, 9,
+		// Right Wing (Count: 4)
+		18, 6, 5,
+		18, 8, 5,
+		18, 8, 10,
+		18, 6, 10,
+		// Rear Flat Middle (Count: 2)
+		4, 5, 7,
+		8, 5, 7,
+		// Center (Count: 4)
+		8, 11, 7,
+		8, 11, 12,
+		8, 10, 12,
+		9, 11, 7,
+		// Front Body (Count: 6)
+		11, 13, 9,
+		16, 13, 9,
+		16, 13, 15,
+		16, 14, 15,
+		16, 14, 10,
+		12, 14, 10,
+		// Captain's Windows (Count: 11)
+		19, 11, 12,	//Back (part 1)
+		19, 11, 13,	//Left (part 1)
+		19, 21, 13,	//Left (part 2)
+		21, 15, 13,	//Front Left
+		21, 15, 22,	//Front Center
+		14, 15, 22,	//Front Right
+		14, 12, 22,	//Right (part 1)
+		20, 12, 22,	//Right (part 2)
+		20, 12, 19,	//Back (part 2)
+		20, 21, 19,	//Roof (part 1)
+		20, 21, 22	//Roof (part 2)
 	};
 
 	//ModelAsset Creation
